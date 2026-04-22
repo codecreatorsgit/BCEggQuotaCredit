83e830063df65cf0cf7800b62e0a03138ddff715
Syed Qamber Hussain <sqamber@codecreators.ca>
2026-04-22 06:36:35 -0700

    fixes

--- src/webparts/business/cppservice.ts
+++ src/webparts/business/cppservice.ts
@@ -7,26 +7,32 @@ export class CPPService {
         this.api = new ApiService(context)
     }
 
-    static validateBarn(premiseID:string): boolean {
+    static validateBarn(barnsTotal: any, BarnTable: any): boolean {
+        const barnTableSet = new Set(
+            BarnTable.map((b: { Bc_barn: any; }) => String(b.Bc_barn))
+        );
+
+        return barnsTotal.every((barn: { BarnNumber: any; }) =>
+            barnTableSet.has(String(barn.BarnNumber))
+        );
+    }
+
+    static validateBarnCapacity(premiseID: string): boolean {
         return true;
     }
 
-    static validateBarnCapacity(premiseID:string): boolean {
-        return true;
-    }
+    static calculateWeekOneNineDate = (date: string): Date => {
+        if (!date) return new Date();
+        const _date = new Date(date);
+        _date.setDate(_date.getDate() + (19 * 7));
+        return _date;
+    };
 
- static calculateWeekOneNineDate = (date: string):Date => {
-    if (!date) return new Date();
-    const _date = new Date(date);
-    _date.setDate(_date.getDate() + (19*7));
-    return _date;
-  };
-
-  static calculateWeekSevenTwoDate = (date: string):Date => {
-    if (!date) return new Date();
-    const _date = new Date(date);
-    _date.setDate(_date.getDate() + (72*7));
-    return _date;
-  };
+    static calculateWeekSevenTwoDate = (date: string): Date => {
+        if (!date) return new Date();
+        const _date = new Date(date);
+        _date.setDate(_date.getDate() + (72 * 7));
+        return _date;
+    };
 
 }
